0bbf8362ccf7e00908780ef5087d3ecec05cf743
richard200001 <vegar7639@gmail.com>
2022-09-08 18:36:02 -0500

    fixed redirect

--- server/app.js
+++ server/app.js
@@ -21,5 +21,8 @@ app.use(fileUpload({
 //Routes
 app.use(postsRoutes);
 app.use(express.static(join(__dirname, '../client/build')))
+app.get('*', (req,res) => {
+    res.sendFile(join(__dirname, '../client/build/index.html'))
+})
 
 export default app
